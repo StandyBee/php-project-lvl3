3c39e2c70ff7bda7f34275063c90307da6830a47
Anton <afisak94@gmail.com>
2022-08-11 10:55:58 +0300

    fix env

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'https://php-page-analyzer-project.herokuapp.com/'),
+    'url' => env('APP_URL', 'laravel'),
 
     'asset_url' => env('ASSET_URL', null),
 
