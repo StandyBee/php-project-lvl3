b1027e711300aa0d9c971725d207f3d22b90024e
Anton <afisak94@gmail.com>
2022-08-11 10:52:29 +0300

    fix env

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://php-page-analyzer-project.herokuapp.com/'),
 
     'asset_url' => env('ASSET_URL', null),
 
