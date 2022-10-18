d4ba3a3f0f2718ce4e260f7f512bce9e283ad14f
Anton Fisak <afisak94@gmail.com>
2022-10-18 16:53:29 +0300

    add fixed files

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title></title>
+    <title>Анализатор страниц</title>
 
 <link href="https://getbootstrap.com/docs/4.5/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
 
