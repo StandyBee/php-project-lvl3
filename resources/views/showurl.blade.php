9feea9adf11dd3c2ba22f94bc5af2a518fd66c19
Anton Fisak <afisak94@gmail.com>
2022-10-18 17:39:07 +0300

    fix showurl

--- resources/views/showurl.blade.php
+++ resources/views/showurl.blade.php
@@ -52,8 +52,8 @@
                     <td>{{ $check->id }}</td>
                     <td>{{ $check->status_code }}</td>
                     <td>{{ $check->h1 }}</td>
-                    <td>{{ Str::limit($check->keywords, 2) }}</td>
-                    <td>{{ Str::limit($check->description, 2) }}</td>
+                    <td>{{ $check->keywords }}</td>
+                    <td>{{ $check->description }}</td>
                     <td>{{ $check->created_at }}</td>
                 </tr>
 
