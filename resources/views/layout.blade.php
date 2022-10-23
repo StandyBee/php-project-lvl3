f3c61c9e809b4dcfee674535022f452a5e408a5d
Anton Fisak <afisak94@gmail.com>
2022-10-23 13:47:51 +0300

    fix views

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,7 +1,7 @@
 <html>
     <head>
         <meta charset="utf-8">
-
+        <title>Анализатор страниц</title>
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
 
     <body class="min-vh-100 d-flex flex-column">
