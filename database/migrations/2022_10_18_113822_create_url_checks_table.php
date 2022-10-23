757489ef1c80fb25f0868d08fdfa6e97f2ebac65
Anton Fisak <afisak94@gmail.com>
2022-10-23 14:11:09 +0300

    fix migrations

--- database/migrations/2022_10_18_113822_create_url_checks_table.php
+++ database/migrations/2022_10_18_113822_create_url_checks_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUrlCheckTable extends Migration
+class CreateUrlChecksTable extends Migration
 {
     /**
      * Run the migrations.
