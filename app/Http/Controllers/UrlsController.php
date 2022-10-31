29dcca36778574aee7c0e33abd0587bd8b2b0e1b
fisak <afisak94@gmail.com>
2022-10-31 19:40:05 +0300

    fix controllers

--- app/Http/Controllers/UrlsController.php
+++ app/Http/Controllers/UrlsController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Contracts\Routing\ResponseFactory;
 use Illuminate\Http\Response;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
