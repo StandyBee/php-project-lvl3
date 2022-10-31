44de2735b608691d8f8d9d233a73d2343d259b78
fisak <afisak94@gmail.com>
2022-10-31 18:09:28 +0300

    fix controllers

--- app/Http/Controllers/UrlsController.php
+++ app/Http/Controllers/UrlsController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-//use Illuminate\Http\RedirectResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Carbon\Carbon;
