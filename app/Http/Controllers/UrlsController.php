5b4c6c095d0bc2513c6a11c216ec0f6c9064dc62
fisak <afisak94@gmail.com>
2022-10-31 18:07:20 +0300

    fix controllers

--- app/Http/Controllers/UrlsController.php
+++ app/Http/Controllers/UrlsController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\RedirectResponse;
+//use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Carbon\Carbon;
