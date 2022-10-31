3f5e9f381a46ad4e312c84e460f04ad581904a25
fisak <afisak94@gmail.com>
2022-10-31 16:46:58 +0300

    fix table

--- resources/views/showurl.blade.php
+++ resources/views/showurl.blade.php
@@ -40,8 +40,8 @@
                     <td>{{ $check->id }}</td>
                     <td>{{ $check->status_code }}</td>
                     <td>{{ $check->h1 }}</td>    
-                    <td>{{ Str::limit($check->title, 20) }}</td>
-                    <td>{{ Str::limit($check->description, 20) }}</td>
+                    <td>{{ Str::limit($check->title, 30) }}</td>
+                    <td>{{ Str::limit($check->description, 30) }}</td>
                     <td>{{ $check->created_at }}</td>
                 </tr>
                 @endforeach
