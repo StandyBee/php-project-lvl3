d1ef2b9b8a1104d3f05d58366f5ff3f35a1ed635
fisak <afisak94@gmail.com>
2022-11-21 13:38:31 +0300

    fix tests

--- tests/Feature/UrlsCheckControllerTest.php
+++ tests/Feature/UrlsCheckControllerTest.php
@@ -2,46 +2,43 @@
 
 namespace Tests\Feature;
 
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Http;
-use Carbon\Carbon;
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
-use WithoutMiddleware;
+use Illuminate\Support\Facades\DB;
+use Carbon\Carbon;
 
-class UrlsCheckControllerTest extends TestCase
+class UrlCheckControllerTest extends TestCase
 {
-    private int $id;
-    private string $url;
-    public function setUp(): void
-    {
-        parent::setUp();
-
-        $this->url = 'https://google.com';
-        $data = ['name' => $this->url];
-        $this->id = DB::table('urls')->insertGetId($data);
-    }
-
     public function testStore(): void
     {
         $this->withoutMiddleware();
-        $fakeHtml = file_get_contents(__DIR__ . "/../Fixtures/fake.html");
-        $name = DB::table('urls')->where('id', '=', $this->id)->value('name');
-        Http::fake(['google.com/*' => Http::response($fakeHtml, 200)]);
-
-        $response = $this->post(route('urls.checks.store', $this->id));
-        $response->assertSessionHasNoErrors();
-        $response->assertRedirect();
         $data = [
-            'url_id' => $this->id,
-            'status_code' => 200,
-            'h1' => 'h1',
-            'title' => 'title',
-            'description' => 'description',
+            'name' => 'https://google.com',
             'created_at' => Carbon::now(),
         ];
+        $id = DB::table('urls')->insertGetId($data);
 
-        $this->assertDatabaseHas('url_checks', $data);
+        $fakeHtml = file_get_contents(__DIR__ . '/../Fixtures/fake.html');
+
+        if ($fakeHtml === false) {
+            throw new \Exception('failed to connect');
+        }
+
+        Http::fake([
+            $data['name'] => Http::response($fakeHtml, 200)
+        ]);
+
+        $response = $this->post(route('urls.checks.store', 1));
+        $response->assertSessionHasNoErrors();
+        $response->assertRedirect();
+
+        $expected = [
+            'url_id' => $id,
+            'status_code' => 200,
+            'h1' => null,
+            'title' => 'Google',
+            'description' => null,
+        ];
+        $this->assertDatabaseHas('url_checks', $expected);
     }
 }
