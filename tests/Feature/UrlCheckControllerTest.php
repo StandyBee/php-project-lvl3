2e7c7f13724ba9ea9d945229795a9a4058a08091
fisak <afisak94@gmail.com>
2022-12-03 14:59:16 +0300

    fix test

--- tests/Feature/UrlCheckControllerTest.php
+++ tests/Feature/UrlCheckControllerTest.php
@@ -9,14 +9,21 @@ use Carbon\Carbon;
 
 class UrlCheckControllerTest extends TestCase
 {
-    public function testStore(): void
+    private int $id;
+    private array $data;
+
+    public function setUp(): void
     {
-        $data = [
+        parent::setUp();
+        $this->data = [
             'name' => 'https://google.com',
             'created_at' => Carbon::now(),
         ];
-        $id = DB::table('urls')->insertGetId($data);
+        $this->id = DB::table('urls')->insertGetId($this->data);
+    }
 
+    public function testStore(): void
+    {
         $fakeHtml = file_get_contents(__DIR__ . '/../Fixtures/fake.html');
 
         if (!$fakeHtml) {
@@ -24,15 +31,15 @@ class UrlCheckControllerTest extends TestCase
         }
 
         Http::fake([
-            $data['name'] => Http::response($fakeHtml, 200)
+            $this->data['name'] => Http::response($fakeHtml, 200)
         ]);
 
-        $response = $this->post(route('urls.checks.store', $id));
+        $response = $this->post(route('urls.checks.store', $this->id));
         $response->assertSessionHasNoErrors();
         $response->assertRedirect();
 
         $expected = [
-            'url_id' => $id,
+            'url_id' => $this->id,
             'status_code' => 200,
             'h1' => 'h1',
             'title' => 'title',
